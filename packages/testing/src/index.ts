cde6e55a00e34c199d4f7e7e2bc3b8bfea7f4762
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-11-06 16:53:35 +0000

    chore(release): testing package

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,1 +1,13 @@
-export * as Jest from './jest'
+import type { JestConfigWithTsJest } from "ts-jest";
+
+export const jestConfig = (): JestConfigWithTsJest => {
+    return {
+        preset: "ts-jest",
+        testEnvironment: "jsdom",
+        transform: {
+            "^.+\\.jsx?$": "babel-jest",
+            "^.+\\.tsx?$": "ts-jest",
+        },
+        transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    };
+};
